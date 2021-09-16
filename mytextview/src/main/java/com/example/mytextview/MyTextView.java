89395110c2710a11b26866b3fb88ebc78d7d43dd
idolone <idolone@163.com>
2021-09-16 10:36:50 +0800

    Initial commit

---
+++ mytextview/src/main/java/com/example/mytextview/MyTextView.java
@@ -0,0 +1,28 @@
+package com.example.mytextview;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MyTextView extends androidx.appcompat.widget.AppCompatTextView{
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+
+    }
+}
